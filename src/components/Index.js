31b392134d574d13f554b54d05cb7eacc10d2a9d
mustaalikanchwala <kanchwalamustaali@gmail.com>
2025-08-08 11:27:23 +0530

    Fix Exports

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,3 +1,6 @@
-import TodoForm from "./TodoForm";
-import TodoItem from "./TodoItem";
-export {TodoForm,TodoItem}
+// import TodoForm from "./TodoForm";
+// import TodoItem from "./TodoItem";
+// export {TodoForm,TodoItem}
+
+export { default as TodoForm } from './TodoForm';
+export { default as TodoItem } from './TodoItem';
